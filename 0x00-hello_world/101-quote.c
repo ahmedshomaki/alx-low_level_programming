ab9932e98f1a120e848655b36ef1cf8c620b6fba
Ahmed Shomaki <ahmedshomaki@gmail.com>
2022-09-08 16:11:17 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,7 @@
 * Return: Always 0 (Success)
 */
 int main(void)
-{	
-
-	system("echo and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	return (0);
+{
+	system("echo and that piece of art is useful - Dora Korpar, 2015-10-19");
+	return (1);
 }
