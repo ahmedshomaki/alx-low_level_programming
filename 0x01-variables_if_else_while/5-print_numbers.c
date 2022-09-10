463920256920ed85e36425f8d2717b5a85c9a00a
Ahmed Shomaki <ahmedshomaki@gmail.com>
2022-09-10 13:33:32 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there*/
+/**
+ * main - The entry point of the program
+ *
+ * Return: The function returns integer
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+
+	return (0);
+}
