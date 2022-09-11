17f6f1d5ad6e26540229f63d8abfde1860a3cfa0
Ahmed Shomaki <ahmedshomaki@gmail.com>
2022-09-11 12:22:29 -0700

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there*/
+/**
+ * main - The entry point of the program
+ *
+ * Return: The function returns integer
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i <= 56; i++)
+	{
+		for (j = 49; j <= 57; j++)
+		{
+		putchar(i);
+		putchar(j);
+		if (i != 56 || j != 57)
+		{
+		putchar(44);
+		putchar(32);
+		}
+		}
+	}
+	putchar(10);
+
+	return (0);
+}
