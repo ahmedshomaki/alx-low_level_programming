4da907cb72fc4ba4d94c3bfa0bd3ee965ea6c149
Ahmed Shomaki <ahmedshomaki@gmail.com>
2022-09-12 06:47:34 -0700

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,30 +14,29 @@ int main(void)
 int i, j, z, q;
 
 for (i = 48; i <= 57; i++)
-    {
-        for (j = 48; j <= 57; j++)
-        {
-            for (z = i; z <= 57; z++)
-            {
-                for (q = 48; q <= 57; q++)
-                {
-                    if ( !(i == z && j == q) )
-                    {
-                        if (q - j >= 1)
-                        {
-                        putchar(i);
-                        putchar(j);
-                        putchar(32);
-                        putchar(z);
-                        putchar(q);
-                        putchar(44);
-                        putchar(32);
-                        }
-                    }
-                }
-            }
-            
-        }
+{
+	for (j = 48; j <= 57; j++)
+	{
+		for (z = i; z <= 57; z++)
+		{
+			for (q = 48; q <= 57; q++)
+			{
+				if (!(i == z && j == q))
+				{
+					if (q - j >= 1)
+					{
+					putchar(i);
+					putchar(j);
+					putchar(32);
+					putchar(z);
+					putchar(q);
+					putchar(44);
+					putchar(32);
+					}
+				}
+			}
+		}
+	}
 }
 putchar(10);
 
