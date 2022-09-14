5e73ca181b0e8d4df92a5a68791e7460a5b87ea5
Ahmed Shomaki <ahmedshomaki@gmail.com>
2022-09-14 10:52:48 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* main - the program entry point
+*
+* Description: this is the first thing in the program
+* Return: it returns 0 if no errors and other values if errors
+*/
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
