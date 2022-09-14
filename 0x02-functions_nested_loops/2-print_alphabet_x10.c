c41f7b3f30e77abef660755103f85acca1a69241
Ahmed Shomaki <ahmedshomaki@gmail.com>
2022-09-14 12:37:44 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+
+
+/**
+* print_alphabet - the function entry point
+*
+* Return: it is void dont return anything
+*/
+
+void print_alphabet(void)
+{
+	int i,j;
+for (j = 1; j <= 5; j++)
+{
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+}
