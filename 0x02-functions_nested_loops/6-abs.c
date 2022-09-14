ba78bd008c0b0a035601c208608d3030e1f99aff
Ahmed Shomaki <ahmedshomaki@gmail.com>
2022-09-14 14:10:24 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _abs - the function entry point
+*@n : integer containing the asci code
+*
+* Return: 1 , 0 or -1
+*/
+
+int _abs(int n)
+{
+	n = abs(n);
+	return n;
+}
