c183bfc9a3a297aa6d7532b6fdaf6f9806c4f904
Ahmed Shomaki <ahmedshomaki@gmail.com>
2022-09-14 14:33:34 -0700

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_last_digit - the function entry point
+*@n : integer containing the asci code
+*
+* Return: integer
+*/
+
+int print_last_digit(int n)
+{
+	n = n % 10;
+	_putchar(n);
+	return (n);
+}
