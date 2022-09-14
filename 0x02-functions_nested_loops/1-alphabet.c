3fd1be8ae7becfd8c02e790a7e51207034b11fdd
Ahmed Shomaki <ahmedshomaki@gmail.com>
2022-09-14 12:06:32 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+# include "main.h"
+
+/**
+* main - the program entry point
+*
+* Description: this is the first thing in the program
+* Return: it returns 0 if no errors and other values if errors
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
