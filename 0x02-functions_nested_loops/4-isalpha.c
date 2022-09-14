e4e3b86e0bd079e1128eba6f7e70f5d5260feb10
Ahmed Shomaki <ahmedshomaki@gmail.com>
2022-09-14 13:33:36 -0700

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* _isalpha - the function entry point
+*@c : integer containing the asci code
+*
+* Return: 1 and 0
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 97)
+	{
+		if (c <= 122)
+		{
+		return (1);
+		}
+	}else if (c >= 65)
+	{
+		if (c <= 90)
+		{
+		return (1);
+		}
+	}
+return (0);
+}
