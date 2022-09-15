d5f5b94a5602f19f8a1ada45e16d4ca2742417ae
Ahmed Shomaki <ahmedshomaki@gmail.com>
2022-09-15 04:00:05 -0700

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+
+/**
+* mul - the function entry point
+*@a b : integers
+*
+* Return: the multiplication of two numbers
+*/
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
