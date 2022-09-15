925f69796d8c05f615c834b2199312663f66da20
Ahmed Shomaki <ahmedshomaki@gmail.com>
2022-09-15 04:56:26 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_numbers - the function entry point
+*
+* Return: void function dont return anything
+*/
+
+void print_numbers(void)
+{
+int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+_putchar('\n');
+}
