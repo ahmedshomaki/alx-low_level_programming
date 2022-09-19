917c0487ada2bb579afccba3a72f2d656b59e3b3
Ahmed Shomaki <ahmedshomaki@gmail.com>
2022-09-19 13:36:42 -0700

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*reset_to_98 - void function the change a variable value
+*@n : a pointer
+*
+*Return : void function does not return any value
+*/
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+
+}
