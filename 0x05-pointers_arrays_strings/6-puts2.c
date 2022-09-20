1bdd20d0c944f77831a493b6d691abaf0e1f8ba4
Ahmed Shomaki <ahmedshomaki@gmail.com>
2022-09-20 13:29:01 -0700

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*print_rev - funcition print reverse
+*@s: char variable which is a pointer
+*
+*Return: void function doesnot return any value
+*/
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	i = i - 1;
+	for (i = 0; i <= i; i+=2)
+	{
+		printf("%c", *(s + i));
+	}
+	printf("\n");
+}
