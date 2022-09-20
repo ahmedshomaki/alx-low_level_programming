f574306403fce1de334252f5912ffe5a1df1fe39
ahmedshomaki <64606777+ahmedshomaki@users.noreply.github.com>
2022-09-20 23:07:58 +0200

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,25 +1,28 @@
-#include "main.h"
-#include <stdio.h>
+#include "holberton.h"
 
 /**
-*rev_string - funcition print reverse
-*@s: char variable which is a pointer
-*
-*Return: void function doesnot return any value
-*/
+ * rev_string - rev string
+ * @s: string
+ */
 
 void rev_string(char *s)
 {
-	int i = 0;
+	char *t = s;
+	char n[1000];
+	short c = 0;
 
-	while (*(s + i) != '\0')
+	while (*s != '\0')
 	{
-		i++;
+		n[c] = *s;
+		s++;
+		c++;
 	}
-	i = i - 1;
-	for (i = i; i >= 0; i--)
+	c = 0;
+
+	while (s > t)
 	{
-		printf("%c", *(s + i));
+		s--;
+		*s = n[c];
+		c++;
 	}
-	printf("\n");
 }
