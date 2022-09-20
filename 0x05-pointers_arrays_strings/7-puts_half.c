aec7d36cee26e983ef916f147e00253049165e35
Ahmed Shomaki <ahmedshomaki@gmail.com>
2022-09-20 13:44:55 -0700

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*void puts_half - funcition print reverse
+*@s: char variable which is a pointer
+*
+*Return: void function doesnot return any value
+*/
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int j = 0;
+
+	while (*(str + j) != '\0')
+	{
+		j++;
+	}
+	if (i %  2 == 0)
+	{
+		for (i = j / 2 ; i <= j - 1; i--)
+        	{
+                printf("%c", *(str + i));
+        	}
+		printf("\n");
+
+	} else
+	{
+		for (i = (j - 1) / 2 ; i <= j - 1; i--)
+		{
+			printf("%c", *(str + i));
+		}
+		printf("\n");
+	}
+}
