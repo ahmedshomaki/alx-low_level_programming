4f8c5e4a4af4e189366b3bc2ad08824a6ace1590
ahmedshomaki <64606777+ahmedshomaki@users.noreply.github.com>
2022-09-20 12:52:00 +0200

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdio.h>
- 
+
 /**
+*_puts - function print string
+*@str: pointer variable
 *
-*
-*
-*
+*Return: void function dont return values
 */
 
 void _puts(char *str)
