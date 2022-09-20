37d9e51a7ef7977ff3950a3b19b4a89ca762d903
Ahmed Shomaki <ahmedshomaki@gmail.com>
2022-09-20 11:33:41 -0700

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,23 +3,23 @@
 
 /**
 *print_rev - funcition print reverse
-*@s: char variable which is a pointer 
+*@s: char variable which is a pointer
 *
 *Return: void function doesnot return any value
 */
 
 void print_rev(char *s)
 {
-    int i = 0;
+	int i = 0;
 
-    while (*(s+i) != '\0')
-    {
-        i++;
-    }
-    i = i - 1;
-    for (i = i; i >= 0; i--)
-    {
-        printf("%c",*(s+i));
-    }
-    printf("\n");
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	i = i - 1;
+	for (i = i; i >= 0; i--)
+	{
+		printf("%c", *(s + i));
+	}
+	printf("\n");
 }
