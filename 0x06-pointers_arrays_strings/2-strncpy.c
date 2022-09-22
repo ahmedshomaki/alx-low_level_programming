1d5df0352a5a4827bcc66b6af4f84e0f1cb23c65
ahmedshomaki <64606777+ahmedshomaki@users.noreply.github.com>
2022-09-22 13:55:59 +0200

    Create 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+*_strcat - function of pointers
+*@dest: pointer to the first string
+*@src: pointer to the second string
+*@n: Integer
+*
+*Return: return p which is a pointer
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *p;
+
+	p = strncpy(dest, src, n);
+	return (p);
+}
