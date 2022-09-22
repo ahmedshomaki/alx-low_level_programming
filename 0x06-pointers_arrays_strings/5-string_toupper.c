d2e69abdbad673870bdc922c3c7d39a995ecc3c3
ahmedshomaki <64606777+ahmedshomaki@users.noreply.github.com>
2022-09-22 17:41:51 +0200

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,13 +1,25 @@
 #include "main.h"
 
 /**
+*string_toupper - char function
+*@n: n is a char variable
 *
-*
-*
-*
+*Return: it returns a pointer
 */
 
 char *string_toupper(char *n)
 {
+        char b[1000];
+        int i = 0 , j = 0;
 
+        while (*n != '\0')
+        {
+                if (*n > 90)
+                {
+                        *n = *n - 32;
+                }
+                n++;    
+        }
+
+return (n-1);
 }
