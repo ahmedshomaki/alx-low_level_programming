c8c0a5c497bdb561196dc4ab1b68715d1c0c2e3c
ahmedshomaki <64606777+ahmedshomaki@users.noreply.github.com>
2022-09-22 12:56:51 +0200

    Create 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*_strcat - function of pointers
+*@dest:
+*@src:
+*
+*Return: return p which is
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	char *p;
+	p = strcat(dest,src);
+	return (p);
+}
